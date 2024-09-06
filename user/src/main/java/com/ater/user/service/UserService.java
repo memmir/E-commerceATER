71696d1e9fbc6d30366747ee576dc015b5601ba2
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-09-06 16:19:56 +0300

    Created getAllUsers method.

--- user/src/main/java/com/ater/user/service/UserService.java
+++ user/src/main/java/com/ater/user/service/UserService.java
@@ -1,13 +1,24 @@
 package com.ater.user.service;
 
+import com.ater.user.dto.UserDto;
+import com.ater.user.dto.UserDtoConverter;
 import com.ater.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final UserDtoConverter userDtoConverter;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, UserDtoConverter userDtoConverter) {
         this.userRepository = userRepository;
+        this.userDtoConverter = userDtoConverter;
+    }
+
+    public List<UserDto> getAllUsers(){
+        return userRepository.findAll().stream().map(x -> userDtoConverter.convert(x)).collect(Collectors.toList());
     }
 }
