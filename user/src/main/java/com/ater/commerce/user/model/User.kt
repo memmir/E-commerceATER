017953985264de11189263a4b69bdd10f7fbc81b
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-09-08 18:32:08 +0300

    Added table name user to "user". beacuse of user is a keyword on SQL.

--- user/src/main/java/com/ater/commerce/user/model/User.kt
+++ user/src/main/java/com/ater/commerce/user/model/User.kt
@@ -1,11 +1,9 @@
-package com.ater.user.model
+package com.ater.commerce.user.model
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 
 @Entity
+@Table(name = "\"user\"")
 data class User(
     @field:Id
     @field:GeneratedValue(strategy = GenerationType.IDENTITY)
