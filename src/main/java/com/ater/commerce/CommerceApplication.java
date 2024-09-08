0574f3db139cbb0e13cb68e60dba8fdbfeef9432
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-09-08 18:30:47 +0300

    Added ComponentScan annotation fo user project.

--- src/main/java/com/ater/commerce/CommerceApplication.java
+++ src/main/java/com/ater/commerce/CommerceApplication.java
@@ -2,8 +2,11 @@ package com.ater.commerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.ater.commerce.user"})
 public class CommerceApplication {
 
 	public static void main(String[] args) {
